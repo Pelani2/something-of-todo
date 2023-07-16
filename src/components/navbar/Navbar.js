81cdd16c47c1a617802f8471a2a9964405b5a823
Igor Zupunski <ipatheword@live.com>
2023-07-16 12:56:12 +0200

    applied some styling to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,10 +2,16 @@ import React from "react";
 import "../navbar/NavbarStyles.css";
 import LoginAndSignupButton from "../buttons/login-and-signup/LoginAndSignupButton";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/images/icons/logo.png";
 
 export default function Navbar() {
     return(
         <nav className="navbar">
+            <img 
+                src={Logo}
+                alt="navbarlogo"
+                className="navbar__logo"
+            />
             <ul className="navbar__links">
                 <li>
                     <Link to="/">
@@ -29,8 +35,14 @@ export default function Navbar() {
                 </li>
             </ul>
             <div className="navbar__buttons">
-                <LoginAndSignupButton text="Log In" />
-                <LoginAndSignupButton text="Sign Up" />
+                <LoginAndSignupButton 
+                    text="Log In"
+                    className="login-signup-button"
+                />
+                <LoginAndSignupButton 
+                    text="Sign Up" 
+                    className="login-signup-button"
+                />
             </div>
         </nav>
     );
