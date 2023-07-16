7dc4beb69c1cdced6ba6d3db7ac58b2c1da70190
Igor Zupunski <ipatheword@live.com>
2023-07-16 12:36:05 +0200

    started logins and signups buttons

---
+++ src/components/buttons/login-and-signup/LoginAndSignupButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function LoginAndSignupButton({
+    text
+}) {
+    return(
+        <button>
+            {text}
+        </button>
+    )
+}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "../navbar/NavbarStyles.css";
+import LoginAndSignupButton from "../buttons/login-and-signup/LoginAndSignupButton";
 
 export default function Navbar() {
     return(
         <nav>
-            Navbar
+            <ul>
+                <li>
+                    Home    
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Services
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+            <div>
+                <LoginAndSignupButton text="Log In" />
+                <LoginAndSignupButton text="Sign Up" />
+            </div>
         </nav>
     );
 }
