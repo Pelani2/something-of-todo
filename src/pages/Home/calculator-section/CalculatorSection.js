b2835f3141fa60e70b9e7160d7d4d4c649215c90
Igor Zupunski <ipatheword@live.com>
2023-07-17 12:49:17 +0200

    adjusted button position

--- src/pages/Home/calculator-section/CalculatorSection.js
+++ src/pages/Home/calculator-section/CalculatorSection.js
@@ -4,8 +4,13 @@ import CalculatorButton from "../../../components/buttons/calculator/CalculatorB
 
 export default function CalculatorSection() {
     return(
-        <div>
-            <CalculatorButton text="Take me to Calculator" />
+        <div className="calculator-section" >
+            <p>
+                Welcome to our site with random things put in here just to make it work. It's not much but it's something "going to be something". Try to have fun.
+            </p>
+            <div className="calculator-section__buttons">   
+                <CalculatorButton text="Take me to Calculator" />
+            </div>
         </div>
     );
 }
