6c1a7926e8d679dd0e8a8e27b827194ed10efb99
Igor Zupunski <ipatheword@live.com>
2023-07-17 12:52:05 +0200

    added routing for calculator

---
+++ src/pages/calculator/Calculator.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CalculatorStyles.css";
+
+export default function Calculator() {
+    return(
+        <div className="calculator-container">
+
+        </div>
+    );
+}
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Calculator from "../pages/calculator/Calculator";
 
 const AppRoutes = createBrowserRouter([
     {
         path: "/",
         element: <Home />
+    },
+    {
+        path: "/calculatorpage",
+        element: <Calculator />
     }
+    
 ]);
 
 export default AppRoutes;
