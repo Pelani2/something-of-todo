5e54f1c5c486368ab392d0034cc87aa013f32ffb
Igor Zupunski <ipatheword@live.com>
2023-07-17 13:16:19 +0200

    started functions for calculator

--- src/pages/calculator/Calculator.js
+++ src/pages/calculator/Calculator.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CalculatorStyles.css";
 
 export default function Calculator() {
+    const [displayValue, setDisplayValue] = useState("0");
+    const [expression, setExpression] = useState("");
+
+    const handleDigitClick = (digit) => {
+        setDisplayValue((prevDisplayValue) => prevDisplayValue === "0" ? digit : prevDisplayValue + digit);
+    }
+
+    const handleOperatorClick = (operator) => {
+        setExpression((prevExpression) => prevExpression + displayValue + operator);
+        setDisplayValue("0");
+    }
+
     return(
         <div className="calculator-container">
 
